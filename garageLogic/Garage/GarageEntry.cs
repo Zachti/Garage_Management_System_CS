e972b4fee83209044f2eaf19f152dd00c2cb5680
Zachti <114566720+Zachti@users.noreply.github.com>
2024-06-05 17:29:08 +0300

    minor changes

--- garageLogic/Garage/GarageEntry.cs
+++ garageLogic/Garage/GarageEntry.cs
@@ -1,6 +1,7 @@
-namespace Garage
-{
+namespace Garage {
+    
     internal record CreateGarageEntryInput (Vehicle i_Vehicle, Owner i_Owner);
+    
     internal class GarageEntry (CreateGarageEntryInput i_Dto) {
         public Vehicle Vehicle { get; set; } = i_Dto.i_Vehicle;
         public Owner Owner { get; set; } = i_Dto.i_Owner;
