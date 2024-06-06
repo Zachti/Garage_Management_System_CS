c6d3a1161e0e0fbf04655119aa28aa79b91e96e2
Zachti <114566720+Zachti@users.noreply.github.com>
2024-06-06 12:08:51 +0300

    added welcome logo , fixed value out of range exception bug

--- garageLogic/Exceptions/ValueOutOfRangeException.cs
+++ garageLogic/Exceptions/ValueOutOfRangeException.cs
@@ -3,7 +3,7 @@ namespace Garage {
     public class ValueOutOfRangeException : Exception
     {
         
-        internal ValueOutOfRangeException(Exception i_InnerException, float i_MaxValue, float i_MinValue) :
+        internal ValueOutOfRangeException(Exception i_InnerException, float i_MinValue, float i_MaxValue) :
             base($"Value was not in the range of {i_MinValue} - {i_MaxValue}", i_InnerException) {}
     }
 }
--- ui/enums/eMainMenuOptions.cs
+++ ui/enums/eMainMenuOptions.cs
@@ -7,7 +7,6 @@ namespace Garage {
         RefuelVehicle,
         ChargeVehicle,
         DisplayFullVehicleDetails,
-        ReturnToMainMenu,
         Exit
     }
 }
