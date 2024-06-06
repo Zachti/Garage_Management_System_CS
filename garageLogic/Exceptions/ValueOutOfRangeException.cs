e1f91074eb2c7c13eef5a8522e6febebb6c7b4e6
Zachti <114566720+Zachti@users.noreply.github.com>
2024-06-06 17:51:11 +0300

    minor change

--- garageLogic/Exceptions/ValueOutOfRangeException.cs
+++ garageLogic/Exceptions/ValueOutOfRangeException.cs
@@ -4,6 +4,6 @@ namespace Garage {
     {
         
     internal ValueOutOfRangeException(float i_Value, float i_Min, float i_Max, string? additionalMessage = null)
-        : base($"{additionalMessage ?? string.Empty}\n Value '{i_Value}' is out of range. Allowed range: [{i_Min}, {i_Max}].") {}
+        : base($"{additionalMessage ?? string.Empty}\nValue '{i_Value}' is out of range. Allowed range: [{i_Min}, {i_Max}].") {}
     }
 }
