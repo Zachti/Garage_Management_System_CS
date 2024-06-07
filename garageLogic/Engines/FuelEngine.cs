a0b80229886e3d684c48251d7f35917854f922e7
Zachti <114566720+Zachti@users.noreply.github.com>
2024-06-07 13:27:35 +0300

    added FuelType change when fueling

--- garageLogic/Engines/FuelEngine.cs
+++ garageLogic/Engines/FuelEngine.cs
@@ -1,13 +1,14 @@
 namespace Garage {
     
     internal class FuelEngine(float i_MaxCapacity, float i_CurrentCapacity, eFuelType i_FuelType) : Engine(i_MaxCapacity, i_CurrentCapacity) {
-        private eFuelType FuelType { get; } = i_FuelType;
+        private eFuelType FuelType { get; set; } = i_FuelType;
 
         public override sealed void SupplyEnergy(float i_AmountToAdd, eFuelType? i_FuelType) {
             if (isFuelTypeMismatch((eFuelType)i_FuelType!)) {
                 throw new ArgumentException("Fuel type mismatch");
             }
             base.SupplyEnergy(i_AmountToAdd, i_FuelType);
+            FuelType = (eFuelType)i_FuelType;
         }
 
         private bool isFuelTypeMismatch(eFuelType i_FuelType) {
