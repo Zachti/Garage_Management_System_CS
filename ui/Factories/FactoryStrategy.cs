4eeb4db3544bb44a462670296584e1dd3e086c62
Zachti <114566720+Zachti@users.noreply.github.com>
2024-06-09 08:55:59 +0300

    removed spaces

--- ui/Factories/FactoryStrategy.cs
+++ ui/Factories/FactoryStrategy.cs
@@ -1,14 +1,14 @@
 namespace Garage {
         
-        internal class FactoryStrategy {
-            public static VehicleFactory CreateFactory(eSupportVehicles i_VehicleType) =>
-                i_VehicleType switch {
-                    eSupportVehicles.Motorcycle => new MotorCycleFactory(),
-                    eSupportVehicles.ElectricMotorcycle => new ElectricMotorCycleFactory(),
-                    eSupportVehicles.Car => new CarFactory(),
-                    eSupportVehicles.ElectricCar => new ElectricCarFactory(),
-                    eSupportVehicles.Truck => new TruckFactory(),
-                    _ => throw new ValueOutOfRangeException((float)i_VehicleType, (float)eSupportVehicles.Motorcycle, (float)eSupportVehicles.Truck)
-                };
-        }
+    internal class FactoryStrategy {
+        public static VehicleFactory CreateFactory(eSupportVehicles i_VehicleType) =>
+            i_VehicleType switch {
+                eSupportVehicles.Motorcycle => new MotorCycleFactory(),
+                eSupportVehicles.ElectricMotorcycle => new ElectricMotorCycleFactory(),
+                eSupportVehicles.Car => new CarFactory(),
+                eSupportVehicles.ElectricCar => new ElectricCarFactory(),
+                eSupportVehicles.Truck => new TruckFactory(),
+                _ => throw new ValueOutOfRangeException((float)i_VehicleType, (float)eSupportVehicles.Motorcycle, (float)eSupportVehicles.Truck)
+            };
+    }
 }
